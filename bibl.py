2e1af3273304ba48ef2b1a98cc39766572698e63
sgillies <sean.gillies@gmail.com>
2012-03-01 21:12:10 -0700

    Use unidecode module to convert names to ascii.

--- bibl.py
+++ bibl.py
@@ -3,6 +3,7 @@ from collections import defaultdict
 import csv
 from datetime import datetime
 from jinja2 import Template
+from unidecode import unidecode
 
 # Get template
 template = Template(open("template.html").read())
@@ -10,29 +11,31 @@ template = Template(open("template.html").read())
 bibl = defaultdict(list)
 for row in csv.DictReader(open("bibl.csv")):
     key = unicode(row['short_new'], "utf-8")
-    letter = key[0].capitalize()
+    letter = unidecode(key)[0].capitalize()
     bibl[letter].append(
         (key, 
          unicode(row['long'], "utf-8"), 
          [unicode(s, "utf-8") for s in row['shorts'].split("; ")] ))
 
+import pdb; pdb.set_trace()
+
 for letter in sorted(bibl.keys()):
     items = sorted(bibl[letter])
     html = template.render(
-        keys=sorted(bibl.keys()), 
+        keys=sorted(bibl.keys()),
         letter=letter, 
         items=items, 
-        modified=datetime.now().isoformat()
+        modified=datetime.now().isoformat(),
         ).encode("utf-8")
     sink = open("htdocs/%s.html" % letter, "w")
     sink.write(html)
     sink.close()
 
 html = template.render(
-    keys=sorted(bibl.keys()), 
+    keys=sorted(bibl.keys()),
     letter="Index", 
     items=[], 
-    modified=datetime.now().isoformat()
+    modified=datetime.now().isoformat(),
     ).encode("utf-8")
 sink = open("htdocs/index.html", "w")
 sink.write(html)
